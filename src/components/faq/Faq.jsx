52985297560331d18c7cc59532516011089b39a2
Olya2125 <139589012+Olya2125@users.noreply.github.com>
2025-05-19 17:36:51 +0300

    changed some files

--- src/components/faq/Faq.jsx
+++ src/components/faq/Faq.jsx
@@ -49,7 +49,63 @@ const Faq = () => {
             <div className="faq-container">
                 <h2 className="faq-title">ЧАСТО ЗАДАВАЕМЫЕ ВОПРОСЫ</h2>
                 <h2 className=" faq-title faq-subtitle">FAQ</h2>
+                                <h3 className=" faq-title-visa">О визах США</h3>
+
                 {faqData.map((item, index) => (
+                    <div 
+                        key={index} 
+                        className={`faq-item ${activeIndex === index ? "active" : ""}`}
+                        onClick={() => toggleAnswer(index)}
+                    >
+                        <div className="faq-question">
+                            <span className="faq-text">{item.question}</span>
+                            <span className="faq-arrow">
+                                {activeIndex === index ? (
+                                    <svg 
+                                        width="37"
+                                        height="16"
+                                        viewBox="0 0 37 16"
+                                        fill="none"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path
+                                            d="M1.63965 15L18.6396 1L35.6396 15"
+                                            stroke="#0F0F0F"
+                                            strokeWidth="2"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                        />
+                                    </svg>
+                                ) : (
+                                    <svg
+                                        width="37"
+                                        height="16"
+                                        viewBox="0 0 37 16"
+                                        fill="none"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path
+                                            d="M1.63965 1L18.6396 15L35.6396 1"
+                                            stroke="#0F0F0F"
+                                            strokeWidth="2"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                        />
+                                    </svg>
+                                )}
+                            </span>
+                        </div>
+                        {activeIndex === index && (
+                            <div className="faq-answer">
+                                <p className="faq-text2">{item.answer}</p>
+                            </div>
+                        )}
+                    </div>
+                ))}
+
+                                                <h3 className=" faq-title-visa any"> О других визах</h3>
+
+                                {faqData.map((item, index) => (
                     <div 
                         key={index} 
                         className={`faq-item ${activeIndex === index ? "active" : ""}`}
